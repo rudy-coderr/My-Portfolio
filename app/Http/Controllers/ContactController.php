ef902de0fb5e835bf5086d632565ecb507ac3570
rica016 <jericabuerta@my.cspc.edu.ph>
2026-07-16 23:19:29 +0800

    email message

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,33 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use App\Mail\ContactMail;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Throwable;
 
 class ContactController extends Controller
 {
-   public function send(Request $request)
-{
-    $request->validate([
-        'name'    => 'required|string|max:255',
-        'email'   => 'required|email',
-        'subject' => 'required|string|max:255',
-        'message' => 'required|string',
-    ]);
+    public function send(Request $request)
+    {
+        $request->validate([
+            'name'    => 'required|string|max:255',
+            'email'   => 'required|email|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string',
+        ]);
 
-    try {
-        Mail::to('boringotr@gmail.com')->send(
-            new ContactMail($request->all())
-        );
+        try {
+            Mail::to('boringotr@gmail.com')->send(
+                new ContactMail($request->only([
+                    'name',
+                    'email',
+                    'subject',
+                    'message'
+                ]))
+            );
 
-        return back()->with('success', 'Your message has been sent successfully!');
-    } catch (Throwable $e) {
-        Log::error('MAIL ERROR: '.$e->getMessage());
+            return back()->with('success', 'Your message has been sent successfully!');
+        } catch (Throwable $e) {
 
-        return back()->with('error', $e->getMessage());
+            Log::error('MAIL ERROR', [
+                'message' => $e->getMessage(),
+                'file'    => $e->getFile(),
+                'line'    => $e->getLine(),
+                'trace'   => $e->getTraceAsString(),
+            ]);
+
+            return back()->with('error', 'Failed to send message. Please try again later.');
+        }
     }
-}
 }
